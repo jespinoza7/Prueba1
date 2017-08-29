1f95ff3695ff563cd560ffc182ab83a0a712e521
Alumno <Alumno@ws204-02>
2017-08-29 15:49:08 -0700

    le agregue otro print

--- src/PAQUETE1/hola.java
+++ src/PAQUETE1/hola.java
@@ -5,6 +5,9 @@ public class hola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo.");
+		System.out.println("Hola Mundo 2.");
+		System.out.println("Hola Mundo 3.");
+	}
 	}
 
-}
+
