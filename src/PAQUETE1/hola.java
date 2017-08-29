6b50a0973cd11cb727d4a2ad547cb441480ed1b8
Alumno <Alumno@ws204-02>
2017-08-29 16:07:42 -0700

    le quite prints

--- src/PAQUETE1/hola.java
+++ src/PAQUETE1/hola.java
@@ -6,12 +6,11 @@ public class hola {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo.");
 		System.out.println("Hola Mundo 2.");
-		System.out.println("Hola Mundo 3.");
-		System.out.println("Hola Mundo 3.");
+		
 		
 		int i;
 		for(i = 0; i< 5; i++){
-			System.out.println("Jesusin.");
+			System.out.println("\nJesusin.");
 			
 		}
 	}
