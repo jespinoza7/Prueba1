3602f9951d109f53de6eccfa8f69fd98b7d9d15d
Alumno <Alumno@ws204-02>
2017-08-29 16:02:52 -0700

    le agregue un ciclo.

--- src/PAQUETE1/hola.java
+++ src/PAQUETE1/hola.java
@@ -8,6 +8,12 @@ public class hola {
 		System.out.println("Hola Mundo 2.");
 		System.out.println("Hola Mundo 3.");
 		System.out.println("Hola Mundo 3.");
+		
+		int i;
+		for(i = 0; i< 5; i++){
+			System.out.println("Jesusin.");
+			
+		}
 	}
 	}
 
